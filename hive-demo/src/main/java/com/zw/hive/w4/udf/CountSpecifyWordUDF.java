819a0398b54a1e875073a338e29d057803bf89b2
zhangws <zhangwenshan@hunting4x.com>
2016-09-21 11:04:04 +0800

    Hive Demo

--- hive-demo/src/main/java/com/zw/hive/w4/udf/CountSpecifyWordUDF.java
+++ hive-demo/src/main/java/com/zw/hive/w4/udf/CountSpecifyWordUDF.java
@@ -1,10 +1,24 @@
-package com.zw.hive.w4;
+package com.zw.hive.w4.udf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 /**
  *
  * 计算src中包含word的个数
+ *
+ * <p>
+ *     UDF是作用于单个数据行，产生一个数据行;
+ *     用户必须要继承UDF，且必须至少实现一个evalute方法，该方法并不在UDF中
+ *     但是Hive会检查用户的UDF是否拥有一个evalute方法
+ * </p>
+ * <pre>
+ *     <code>
+ *         add jar /home/zkpk/doc/hive/hive-udf.jar;
+ *         create temporary function countSpecifyWord com.zw.hive.w4.udf.CountSpecifyWordUDF;
+ *         select countSpecifyWord(col) from table_name;
+ *     </code>
+ * </pre>
+ *
  *
  * Created by zhangws on 16/8/27.
  */
