e566fa1b557dd4f0235fd222a1bc9fff7e5b946b
zhangws <zhangwenshan@hunting4x.com>
2016-11-05 17:04:44 +0800

    spark operator's demo

--- spark-demo/src/main/java/com/zw/spark/demo/JavaOperatorDemo.java
+++ spark-demo/src/main/java/com/zw/spark/demo/JavaOperatorDemo.java
@@ -1,19 +1,16 @@
 package com.zw.spark.demo;
 
 import com.google.gson.Gson;
+import org.apache.spark.HashPartitioner;
+import org.apache.spark.Partitioner;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.api.java.function.*;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by zhangws on 16/10/30.
@@ -21,11 +18,32 @@ import java.util.List;
 public class JavaOperatorDemo {
 
     public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName(JavaOperatorDemo.class.getSimpleName())
+                .setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
 //        map();
 //        filter();
-        flatMap();
-//        cartesian();
+//        flatMap();
+//        mapPartitions();
 //        mapPartitionsWithIndex();
+//        sample();
+//        union();
+//        intersection(sc);
+//        distinct(sc);
+//        groupBy(sc);
+//        reduceByKey(sc);
+//        aggregateByKey(sc);
+//        sortByKey(sc);
+//        join(sc);
+//        cogroup(sc);
+//        pipe(sc);
+//        coalesce(sc);
+//        cartesian(sc);
+        repartitionAndSortWithinPartitions(sc);
+
+        sc.close();
     }
 
     public static class User {
@@ -132,7 +150,34 @@ public class JavaOperatorDemo {
     }
 
     static void mapPartitions() {
+        SparkConf conf = new SparkConf().setAppName(JavaOperatorDemo.class.getSimpleName())
+                .setMaster("local");
 
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<String> names = Arrays.asList("张三1", "李四1", "王五1", "张三2", "李四2",
+                "王五2", "张三3", "李四3", "王五3", "张三4");
+
+        JavaRDD<String> namesRDD = sc.parallelize(names, 3);
+        JavaRDD<String> mapPartitionsRDD = namesRDD.mapPartitions(
+                new FlatMapFunction<Iterator<String>, String>() {
+                    int count = 0;
+                    @Override
+                    public Iterator<String> call(Iterator<String> stringIterator) throws Exception {
+                        List<String> list = new ArrayList<String>();
+                        while (stringIterator.hasNext()) {
+                            list.add("分区索引:" + count++ + "\t" + stringIterator.next());
+                        }
+                        return list.iterator();
+                    }
+                }
+        );
+
+        // 从集群获取数据到本地内存中
+        List<String> result = mapPartitionsRDD.collect();
+        result.forEach(System.out::println);
+
+        sc.close();
     }
 
     /**
@@ -166,23 +211,252 @@ public class JavaOperatorDemo {
 
         // 从集群获取数据到本地内存中
         List<String> result = mapPartitionsWithIndexRDD.collect();
-
-        for (String v : result) {
-            System.out.println(v);
-        }
+        result.forEach(System.out::println);
 
         sc.close();
     }
 
-    /**
-     * 笛卡尔积算子
-     */
-    private static void cartesian() {
+    static void sample() {
         SparkConf conf = new SparkConf().setAppName(JavaOperatorDemo.class.getSimpleName())
                 .setMaster("local");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        List<Integer> datas = Arrays.asList(1, 2, 3, 7, 4, 5, 8);
+
+        JavaRDD<Integer> dataRDD = sc.parallelize(datas);
+        JavaRDD<Integer> sampleRDD = dataRDD.sample(false, 0.5, System.currentTimeMillis());
+        sampleRDD.foreach(v -> System.out.println(v));
+
+        sc.close();
+    }
+
+    static void union() {
+        SparkConf conf = new SparkConf().setAppName(JavaOperatorDemo.class.getSimpleName())
+                .setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<String> datas1 = Arrays.asList("张三", "李四");
+        List<String> datas2 = Arrays.asList("tom", "gim");
+
+        JavaRDD<String> data1RDD = sc.parallelize(datas1);
+        JavaRDD<String> data2RDD = sc.parallelize(datas2);
+
+        JavaRDD<String> unionRDD = data1RDD
+                .union(data2RDD);
+
+        unionRDD.foreach(v -> System.out.println(v));
+    }
+
+    static void intersection(JavaSparkContext sc) {
+        List<String> datas1 = Arrays.asList("张三", "李四", "tom");
+        List<String> datas2 = Arrays.asList("tom", "gim");
+
+        sc.parallelize(datas1)
+                .intersection(sc.parallelize(datas2))
+                .foreach(v -> System.out.println(v));
+    }
+
+    static void distinct(JavaSparkContext sc) {
+        List<String> datas = Arrays.asList("张三", "李四", "tom", "张三");
+
+        sc.parallelize(datas)
+                .distinct()
+                .foreach(v -> System.out.println(v));
+    }
+
+    static void groupBy(JavaSparkContext sc) {
+        List<Integer> datas = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        sc.parallelize(datas)
+                .groupBy(new Function<Integer, Object>() {
+                    @Override
+                    public Object call(Integer v1) throws Exception {
+                        return (v1 % 2 == 0) ? "偶数" : "奇数";
+                    }
+                })
+                .collect()
+                .forEach(System.out::println);
+
+
+        List<String> datas2 = Arrays.asList("dog", "tiger", "lion", "cat", "spider", "eagle");
+
+        sc.parallelize(datas2)
+                .keyBy(v1 -> v1.length())
+                .groupByKey()
+                .collect()
+                .forEach(System.out::println);
+    }
+
+    static void reduceByKey(JavaSparkContext sc) {
+        JavaRDD<String> lines = sc.textFile("file:///Users/zhangws/opt/spark-2.0.1-bin-hadoop2.6/README.md");
+
+        JavaRDD<String> wordsRDD = lines.flatMap(new FlatMapFunction<String, String>() {
+
+            private static final long serialVersionUID = 1L;
+
+            public Iterator<String> call(String line) throws Exception {
+                List<String> words = Arrays.asList(line.split(" "));
+                return words.iterator();
+            }
+        });
+
+        JavaPairRDD<String, Integer> wordsCount = wordsRDD.mapToPair(new PairFunction<String, String, Integer>() {
+
+            private static final long serialVersionUID = 1L;
+
+            public Tuple2<String, Integer> call(String word) throws Exception {
+                return new Tuple2<String, Integer>(word, 1);
+            }
+        });
+
+        JavaPairRDD<String, Integer> resultRDD = wordsCount.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+            private static final long serialVersionUID = 1L;
+
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        resultRDD.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+
+            private static final long serialVersionUID = 1L;
+
+            public void call(Tuple2<String, Integer> t) throws Exception {
+                System.out.println(t._1 + "\t" + t._2());
+            }
+        });
+
+        sc.close();
+    }
+
+    static void aggregateByKey(JavaSparkContext sc) {
+        List<Tuple2<Integer, Integer>> datas = new ArrayList<>();
+        datas.add(new Tuple2<>(1, 3));
+        datas.add(new Tuple2<>(1, 2));
+        datas.add(new Tuple2<>(1, 4));
+        datas.add(new Tuple2<>(2, 3));
+
+        sc.parallelizePairs(datas, 2)
+                .aggregateByKey(
+                        0,
+                        new Function2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer v1, Integer v2) throws Exception {
+                                System.out.println("seq: " + v1 + "\t" + v2);
+                                return Math.max(v1, v2);
+                            }
+                        },
+                        new Function2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer v1, Integer v2) throws Exception {
+                                System.out.println("comb: " + v1 + "\t" + v2);
+                                return v1 + v2;
+                            }
+                        })
+                .collect()
+                .forEach(System.out::println);
+    }
+
+    static void sortByKey(JavaSparkContext sc) {
+
+        List<Integer> datas = Arrays.asList(60, 70, 80, 55, 45, 75);
+
+//        sc.parallelize(datas)
+//                .sortBy(new Function<Integer, Object>() {
+//                    @Override
+//                    public Object call(Integer v1) throws Exception {
+//                        return v1;
+//                    }
+//                }, true, 1)
+//                .foreach(v -> System.out.println(v));
+
+        sc.parallelize(datas)
+                .sortBy((Integer v1) -> v1, false, 1)
+                .foreach(v -> System.out.println(v));
+
+        List<Tuple2<Integer, Integer>> datas2 = new ArrayList<>();
+        datas2.add(new Tuple2<>(3, 3));
+        datas2.add(new Tuple2<>(2, 2));
+        datas2.add(new Tuple2<>(1, 4));
+        datas2.add(new Tuple2<>(2, 3));
+
+        sc.parallelizePairs(datas2)
+                .sortByKey(false)
+                .foreach(v -> System.out.println(v));
+    }
+
+    static void join(JavaSparkContext sc) {
+        List<Tuple2<Integer, String>> products = new ArrayList<>();
+        products.add(new Tuple2<>(1, "苹果"));
+        products.add(new Tuple2<>(2, "梨"));
+        products.add(new Tuple2<>(3, "香蕉"));
+        products.add(new Tuple2<>(4, "石榴"));
+
+        List<Tuple2<Integer, Integer>> counts = new ArrayList<>();
+        counts.add(new Tuple2<>(1, 7));
+        counts.add(new Tuple2<>(2, 3));
+        counts.add(new Tuple2<>(3, 8));
+        counts.add(new Tuple2<>(4, 3));
+
+        JavaPairRDD<Integer, String> productsRDD = sc.parallelizePairs(products);
+        JavaPairRDD<Integer, Integer> countsRDD = sc.parallelizePairs(counts);
+
+        productsRDD.join(countsRDD)
+                .foreach(v -> System.out.println(v));
+    }
+
+    static void cogroup(JavaSparkContext sc) {
+        List<Tuple2<Integer, String>> datas1 = new ArrayList<>();
+        datas1.add(new Tuple2<>(1, "苹果"));
+        datas1.add(new Tuple2<>(2, "梨"));
+        datas1.add(new Tuple2<>(3, "香蕉"));
+        datas1.add(new Tuple2<>(4, "石榴"));
+
+        List<Tuple2<Integer, Integer>> datas2 = new ArrayList<>();
+        datas2.add(new Tuple2<>(1, 7));
+        datas2.add(new Tuple2<>(2, 3));
+        datas2.add(new Tuple2<>(3, 8));
+        datas2.add(new Tuple2<>(4, 3));
+
+
+        List<Tuple2<Integer, String>> datas3 = new ArrayList<>();
+        datas3.add(new Tuple2<>(1, "7"));
+        datas3.add(new Tuple2<>(2, "3"));
+        datas3.add(new Tuple2<>(3, "8"));
+        datas3.add(new Tuple2<>(4, "3"));
+        datas3.add(new Tuple2<>(4, "4"));
+        datas3.add(new Tuple2<>(4, "5"));
+        datas3.add(new Tuple2<>(4, "6"));
+
+        sc.parallelizePairs(datas1)
+                .cogroup(sc.parallelizePairs(datas2),
+                        sc.parallelizePairs(datas3))
+                .foreach(v -> System.out.println(v));
+    }
+
+    static void pipe(JavaSparkContext sc) {
+        List<String> datas = Arrays.asList("hi", "hello", "how", "are", "you");
+        sc.parallelize(datas)
+                .pipe("/Users/zhangws/echo.sh")
+                .collect()
+                .forEach(System.out::println);
+    }
+
+    static void coalesce(JavaSparkContext sc) {
+        List<String> datas = Arrays.asList("hi", "hello", "how", "are", "you");
+        JavaRDD<String> datasRDD = sc.parallelize(datas, 4);
+        System.out.println("RDD的分区数: " + datasRDD.partitions().size());
+        JavaRDD<String> datasRDD2 = datasRDD.coalesce(2);
+        System.out.println("RDD的分区数: " + datasRDD2.partitions().size());
+    }
+
+    /**
+     * 笛卡尔积算子
+     */
+    private static void cartesian(JavaSparkContext sc) {
         List<String> names = Arrays.asList("张三", "李四", "王五");
         List<Integer> scores = Arrays.asList(60, 70, 80);
 
@@ -198,8 +472,37 @@ public class JavaOperatorDemo {
                 System.out.println(t._1 + "\t" + t._2());
             }
         });
+    }
 
-        sc.close();
+    static void repartitionAndSortWithinPartitions(JavaSparkContext sc) {
+        List<String> datas = new ArrayList<>();
+        Random random = new Random(1);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100; j++) {
+                datas.add(String.format("product%02d,url%03d", random.nextInt(10), random.nextInt(100)));
+            }
+        }
+        JavaRDD<String> datasRDD = sc.parallelize(datas);
+        JavaPairRDD<String, String> pairRDD = datasRDD.mapToPair((String v) -> {
+            String[] values = v.split(",");
+            return new Tuple2<>(values[0], values[1]);
+        });
+        JavaPairRDD<String, String> partSortRDD = pairRDD.repartitionAndSortWithinPartitions(
+                new Partitioner() {
+
+                    @Override
+                    public int numPartitions() {
+                        return 10;
+                    }
+
+                    @Override
+                    public int getPartition(Object key) {
+                        return Integer.valueOf(((String) key).substring(7));
+                    }
+                }
+        );
+        partSortRDD.collect()
+        .forEach(System.out::println);
     }
 
 }
--- spark-demo/src/main/scala/com/zw/spark/demo/ScalaOperatorDemo.scala
+++ spark-demo/src/main/scala/com/zw/spark/demo/ScalaOperatorDemo.scala
@@ -1,9 +1,10 @@
 package com.zw.spark.demo
 
 import com.google.gson.Gson
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{Partitioner, SparkConf, SparkContext}
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 /**
   * <p>
@@ -26,11 +27,27 @@ import scala.collection.mutable.ArrayBuffer
 object ScalaOperatorDemo {
 
     def main(args: Array[String]): Unit = {
-//        map()
-//        filter
-        flatMap()
-        //    cartesian
-        //    mapPartitionsWithIndex
+        val conf = new SparkConf().setAppName(ScalaOperatorDemo.getClass.getSimpleName).setMaster("local")
+        val sc = new SparkContext(conf)
+
+        //        map()
+        //        filter
+        //        flatMap()
+        //        mapPartitions()
+        //        mapPartitionsWithIndex
+        //        sample()
+        //        union(sc)
+        //        intersection(sc)
+        //        distinct(sc)
+        //        groupBy(sc)
+        //        aggregateByKey(sc)
+        //        sortByKey(sc)
+        //        join(sc)
+        //        cogroup(sc)
+        //        pipe(sc)
+        //        coalesce(sc)
+        //        cartesian(sc)
+        repartitionAndSortWithinPartitions(sc)
     }
 
     class User(var id: Int, var name: String, var pwd: String, var sex: Int)
@@ -64,7 +81,7 @@ object ScalaOperatorDemo {
             })
     }
 
-    def filter() : Unit = {
+    def filter(): Unit = {
         val conf = new SparkConf().setAppName(ScalaOperatorDemo.getClass.getSimpleName).setMaster("local")
         val sc = new SparkContext(conf)
 
@@ -75,7 +92,7 @@ object ScalaOperatorDemo {
             .foreach(println)
     }
 
-    def flatMap() : Unit =  {
+    def flatMap(): Unit = {
         val conf = new SparkConf().setAppName(ScalaOperatorDemo.getClass.getSimpleName).setMaster("local")
         val sc = new SparkContext(conf)
 
@@ -89,36 +106,236 @@ object ScalaOperatorDemo {
             .foreach(println)
     }
 
-    def mapPartitions() : Unit = {
+    def mapPartitions(): Unit = {
+        val conf = new SparkConf().setAppName(ScalaOperatorDemo.getClass.getSimpleName).setMaster("local")
+        val sc = new SparkContext(conf)
 
+        val datas = Array("张三1", "李四1", "王五1", "张三2", "李四2",
+            "王五2", "张三3", "李四3", "王五3", "张三4")
+
+        sc.parallelize(datas, 3)
+            .mapPartitions(
+                n => {
+                    val result = ArrayBuffer[String]()
+                    while (n.hasNext) {
+                        result.append(n.next())
+                    }
+                    result.iterator
+                }
+            )
+            .foreach(println)
     }
 
-    /**
-      * 笛卡尔算子
-      */
-    def cartesianDemo(): Unit = {
+    def sample(): Unit = {
+        val conf = new SparkConf().setAppName(ScalaOperatorDemo.getClass.getSimpleName).setMaster("local")
 
+        val sc = new SparkContext(conf)
+
+        val datas = Array(1, 2, 3, 7, 4, 5, 8)
+
+        sc.parallelize(datas)
+            .sample(withReplacement = false, 0.5, System.currentTimeMillis)
+            .foreach(println)
     }
 
     /**
       * mapPartitionsWithIndex算子
       */
-    def mapPartitionsWithIndexDemo: Unit = {
+    def mapPartitionsWithIndex(): Unit = {
         val conf = new SparkConf().setAppName(ScalaOperatorDemo.getClass.getSimpleName).setMaster("local")
         val sc = new SparkContext(conf)
 
-        val namesRdd = sc.parallelize(Array("张三1", "李四1", "王五1", "张三2", "李四2",
-            "王五2", "张三3", "李四3", "王五3", "张三4"), 3)
+        val datas = Array("张三1", "李四1", "王五1", "张三2", "李四2",
+            "王五2", "张三3", "李四3", "王五3", "张三4")
 
-        val resultRDD = namesRdd.mapPartitionsWithIndex((m, n) => {
-            val result = ArrayBuffer[String]()
-            while (n.hasNext) {
-                result.append("分区索引:" + m + "\t" + n.next())
+        sc.parallelize(datas, 3)
+            .mapPartitionsWithIndex(
+                (m, n) => {
+                    val result = ArrayBuffer[String]()
+                    while (n.hasNext) {
+                        result.append("分区索引:" + m + "\t" + n.next())
+                    }
+                    result.iterator
+                }
+            )
+            .foreach(println)
+    }
+
+    def union(sc: SparkContext): Unit = {
+        val datas1 = Array("张三", "李四")
+        val datas2 = Array("tom", "gim")
+
+        //        sc.parallelize(datas1)
+        //            .union(sc.parallelize(datas2))
+        //            .foreach(println)
+
+        (sc.parallelize(datas1) ++ sc.parallelize(datas2))
+            .foreach(println)
+    }
+
+    def intersection(sc: SparkContext): Unit = {
+        val datas1 = Array("张三", "李四", "tom")
+        val datas2 = Array("tom", "gim")
+
+        sc.parallelize(datas1)
+            .intersection(sc.parallelize(datas2))
+            .foreach(println)
+    }
+
+    def distinct(sc: SparkContext): Unit = {
+        val datas = Array("张三", "李四", "tom", "张三")
+
+        sc.parallelize(datas)
+            .distinct()
+            .foreach(println)
+    }
+
+    def groupBy(sc: SparkContext): Unit = {
+        sc.parallelize(1 to 9, 3)
+            .groupBy(x => {
+                if (x % 2 == 0) "偶数"
+                else "奇数"
+            })
+            .collect()
+            .foreach(println)
+
+        val datas2 = Array("dog", "tiger", "lion", "cat", "spider", "eagle")
+        sc.parallelize(datas2)
+            .keyBy(_.length)
+            .groupByKey()
+            .collect()
+            .foreach(println)
+    }
+
+    def aggregateByKey(sc: SparkContext): Unit = {
+
+        // 合并在同一个partition中的值，a的数据类型为zeroValue的数据类型，b的数据类型为原value的数据类型
+        def seq(a: Int, b: Int): Int = {
+            println("seq: " + a + "\t" + b)
+            math.max(a, b)
+        }
+
+        // 合并在不同partition中的值，a,b的数据类型为zeroValue的数据类型
+        def comb(a: Int, b: Int): Int = {
+            println("comb: " + a + "\t" + b)
+            a + b
+        }
+
+        // 数据拆分成两个分区
+        // 分区一数据: (1,3) (1,2)
+        // 分区二数据: (1,4) (2,3)
+        // zeroValue 中立值，定义返回value的类型，并参与运算
+        // seqOp 用来在一个partition中合并值的
+        // 分区一相同key的数据进行合并
+        // seq: 0	3  (1,3)开始和中位值合并为3
+        // seq: 3   2  (1,2)再次合并为3
+        // 分区二相同key的数据进行合并
+        // seq: 0	4  (1,4)开始和中位值合并为4
+        // seq: 0	3  (2,3)开始和中位值合并为3
+        // comb 用来在不同partition中合并值的
+        // 将两个分区的结果进行合并
+        // key为1的, 两个分区都有, 合并为(1,7)
+        // key为2的, 只有一个分区有, 不需要合并(2,3)
+        sc.parallelize(List((1, 3), (1, 2), (1, 4), (2, 3)), 2)
+            .aggregateByKey(0)(seq, comb)
+            .collect()
+            .foreach(println)
+    }
+
+    def sortByKey(sc: SparkContext): Unit = {
+        sc.parallelize(Array(60, 70, 80, 55, 45, 75))
+            .sortBy(v => v, false)
+            .foreach(println)
+
+        sc.parallelize(List((3, 3), (2, 2), (1, 4), (2, 3)))
+            .sortByKey(true)
+            .foreach(println)
+    }
+
+    def join(sc: SparkContext): Unit = {
+        sc.parallelize(List((1, "苹果"), (2, "梨"), (3, "香蕉"), (4, "石榴")))
+            .join(sc.parallelize(List((1, 7), (2, 3), (3, 8), (4, 3), (5, 9))))
+            .foreach(println)
+    }
+
+    def cogroup(sc: SparkContext): Unit = {
+        val datas1 = List((1, "苹果"),
+            (2, "梨"),
+            (3, "香蕉"),
+            (4, "石榴"))
+
+        val datas2 = List((1, 7),
+            (2, 3),
+            (3, 8),
+            (4, 3))
+
+
+        val datas3 = List((1, "7"),
+            (2, "3"),
+            (3, "8"),
+            (4, "3"),
+            (4, "4"),
+            (4, "5"),
+            (4, "6"))
+
+        sc.parallelize(datas1)
+            .cogroup(sc.parallelize(datas2),
+                sc.parallelize(datas3))
+            .foreach(println)
+    }
+
+    def pipe(sc: SparkContext): Unit = {
+        val data = List("hi", "hello", "how", "are", "you")
+        sc.makeRDD(data)
+            .pipe("/Users/zhangws/echo.sh")
+            .collect()
+            .foreach(println)
+    }
+
+    def coalesce(sc: SparkContext): Unit = {
+        val datas = List("hi", "hello", "how", "are", "you")
+        val datasRDD = sc.parallelize(datas, 4)
+        println("RDD的分区数: " + datasRDD.partitions.length)
+        val datasRDD2 = datasRDD.coalesce(2)
+        println("RDD的分区数: " + datasRDD2.partitions.length)
+    }
+
+    /**
+      * 笛卡尔算子
+      */
+    def cartesian(sc: SparkContext): Unit = {
+        val namesRDD = sc.parallelize(Array("张三", "李四", "王五"))
+        val scoreRDD = sc.parallelize(Array(60, 70, 80))
+
+        namesRDD.cartesian(scoreRDD)
+            .foreach(println)
+    }
+
+    def repartitionAndSortWithinPartitions(sc: SparkContext): Unit = {
+
+        def partitionFunc(key:String): Int = {
+            key.substring(7).toInt
+        }
+
+        val datas = new Array[String](1000)
+        val random = new Random(1)
+        for (i <- 0 until 10; j <- 0 until 100) {
+            val index: Int = i * 100 + j
+            datas(index) = "product" + random.nextInt(10) + ",url" + random.nextInt(100)
+        }
+        val datasRDD = sc.parallelize(datas)
+        val pairRDD = datasRDD.map(line => (line, 1))
+            .reduceByKey((a, b) => a + b)
+//            .foreach(println)
+
+        pairRDD.repartitionAndSortWithinPartitions(new Partitioner() {
+            override def numPartitions: Int = 10
+
+            override def getPartition(key: Any): Int = {
+                val str = String.valueOf(key)
+                str.substring(7, str.indexOf(',')).toInt
             }
-            result.iterator
-        })
-
-        resultRDD.foreach(v => println(v))
+        }).foreach(println)
     }
 
 }
